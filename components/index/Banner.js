58b06232e3d049db708671a83009e737441a9cad
Subhampreet <subhampreet18@gmail.com>
2021-09-07 18:21:51 +0530

    ✨ Banner UI Enhancement

--- components/index/Banner.js
+++ components/index/Banner.js
@@ -8,7 +8,7 @@ function Banner() {
                 <div className="left">
                     <div className="left-body">
                         <h2>Welcome to Pokedex</h2>
-                        <h1>Catch em ! Collect the Strongest and Rarest Pokemon</h1>
+                        <h1>Catch 'em ! Collect the Strongest and <span>Rarest Pokemon</span></h1>
                         <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
                         <div className="button">CATCH TH'M ALL</div>
                     </div>
