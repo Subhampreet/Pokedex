ad143514387d5fd9cd8c8f54c7b21f8fb93245a6
Subhampreet <subhampreet18@gmail.com>
2021-09-17 15:44:04 +0530

    🦊Pokedex UI Update

--- components/pokedex/SearchPoke.js
+++ components/pokedex/SearchPoke.js
@@ -14,25 +14,25 @@ function SearchPoke() {
 
   return (
     <div className="Search">
-      <div>
-        <div>
+      
+      <div className="search-body">
+
+      <div className="search-content">
+        <h3>Pokemon Go ⚡ &apos;</h3>
+        <h1>Gocha Catch them All 🦊 !</h1>
+        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium ⚡, totam rem aperiam, eaque ipsa quae ab 🦔 illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo 🐩.</p>
+      </div>
+
+
+        <div className="container">
           <input
             type="text"
             onChange={(e) => search(e.target.value)}
             className="bg-transparent outline-none"
+            placeholder="Search Pokemon"
           />
-          <div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              width="20"
-            >
-              <path fill="none" d="M0 0h24v24H0z" />
-              <path
-                d="M18.031 16.617l4.283 4.282-1.415 1.415-4.282-4.283A8.96 8.96 0 0 1 11 20c-4.968 0-9-4.032-9-9s4.032-9 9-9 9 4.032 9 9a8.96 8.96 0 0 1-1.969 5.617zm-2.006-.742A6.977 6.977 0 0 0 18 11c0-3.868-3.133-7-7-7-3.868 0-7 3.132-7 7 0 3.867 3.132 7 7 7a6.977 6.977 0 0 0 4.875-1.975l.15-.15z"
-                fill="rgba(0,0,0,0.92)"
-              />
-            </svg>
+          <div className="search-icon">
+            <p>SEARCH</p>
           </div>
         </div>
       </div>
